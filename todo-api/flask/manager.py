a68700e5ff0c8e5afb1b613843d8fda4f7bb14e8
xxyingr <yinger@yingerdeMacBook-Air.local>
2017-12-15 15:30:04 +0000

    add workerdata locally

--- todo-api/flask/manager.py
+++ todo-api/flask/manager.py
@@ -9,7 +9,6 @@ api = Api(app)
 class getRepo(Resource):
     def __init__(self):  # Upon initialisation of the class          
         self.manager = manager  # Init the global manager
-        super(getRepo, self).__init__()  # Initialising the Resource class
         self.reqparser = reqparse.RequestParser()
 
         # For every value coming in JSON, you need an argument
@@ -20,7 +19,7 @@ class getRepo(Resource):
         args = self.reqparser.parse_args()
         if args['pullStatus'] == False:  # Repo hasn't been pulled yet
             print("GOT 1")
-            return {'repo': "https://github.com/{}/{}".format(self.manager.owner, self.manager.repo)}  
+            return {'repo': 'https://github.com/jrios6/Berkeley-AI-PacMan-Lab-1'}  
         if args['pullStatus'] == True:  # Repo has been pulled, can now increment
             self.manager.currNumWorkers += 1
             if self.manager.currNumWorkers == self.manager.numWorkers:
@@ -35,7 +34,6 @@ api.add_resource(getRepo, "/repo", endpoint="repo")
 #  API for obtaining commits and posting the cyclomatic results
 class cycComplexity(Resource):
     def __init__(self):  # Upon initialisation of the class
-        global manager  # Init the global manager
         self.manager = manager  # Init the global manager
         super(cycComplexity, self).__init__()  # Initialising the Resource class
         self.reqparser = reqparse.RequestParser()
@@ -53,21 +51,16 @@ class cycComplexity(Resource):
             return {'sha': -1}
         commitValue = self.manager.commitList[0]  # give next commit in list
         del self.manager.commitList[0]  # Remove item from list of commits to compute
-        print("Sent: {}".format(commitValue))
+        print(commitSha)
         return {'sha':commitValue}
 
 
     def post(self):
-        args = self.reqparser.parse_args()  # parse the arguments from the POST
-        print("Received sha {}".format(args['commitSha']))
-        print("Received complexity {}".format(args['complexity']))
         self.manager.listOfCCs.append({'sha':args['commitSha'], 'complexity':args['complexity']})
-        print(self.manager.listOfCCs)
-        print(self.manager.commitList)
+        print(self.reqparser.parse_args())
+        args = self.reqparser.parse_args()
         if len(self.manager.listOfCCs) == self.manager.totalNumberOfCommits:
             endTime = time.time() - self.manager.startTime
-            print("finished in {} seconds".format(endTime))
-            print(len(self.manager.listOfCCs))
             totalAverageCC = 0
             for x in self.manager.listOfCCs:
                 if x['complexity'] > 0:
@@ -84,41 +77,32 @@ api.add_resource(cycComplexity, "/cyclomatic", endpoint="cyclomatic")
 
 class managerServer():
     def __init__(self):
-        self.numWorkers = input("Enter number of worker nodes: ")
-        # self.repoDirectory = input("Enter the URL for the repository")
-        self.numWorkers = int(self.numWorkers)
+        self.numWorkers = int(input("Enter number of worker nodes: "))
+        
         self.currNumWorkers = 0  # Number of workers who have connected to the manager
         self.startTime = 0.0  # Start time for the timer
-        # request repository info using the github API
-        print("Authenticated Github API requests have a rate limit of 5000 per hour to the Github API")
-        print("Unauthenticated requests have a limit of 60 requests per hour")
-        idgit = input("Type your Github username to use authenticated requests, or press return to use unauthenitcated requests: ")
-        print(len(idgit))
-        if len(idgit) != 0:
-            gitPassword = getpass.getpass("Type your Github password (input is hidden): ")
-        morePages = True  # Loop control variable to check if more pages on github API
-        currentPage = 1  # Current page of github API repo info
-        self.commitList = []  # List containing all commit sha values
-        while morePages:
-            if len(idgit) == 0:
-                r = requests.get("https://api.github.com/repos/{}/{}/commits?page={}&per_page=100".format(currentPage))  
+        
+        currentpage = 1
+        self.commitList = []
+        morePage = True
+        while morePage:
+            commitURL = 'https://api.github.com/repos/jrios6/Berkeley-AI-PacMan-Lab-1/commits?page=' + str(currentpage) + '&per_page=100'
+            response = requests.get(commitURL)
+            data = json.loads(response.text)
+            if len(data) < 2:
+                morePage = False
+                print('No more pages.')
             else:
-                r = requests.get("https://api.github.com/repos/{}/{}/commits?page={}&per_page=100".format(currentPage), auth=(idgit, gitPassword))
-            json_data = json.loads(r.text)
-            if len(json_data) < 2:
-                morePages = False
-                print("All pages iterated through")
-            else:
-                for x in json_data:
-                    self.commitList.append(x['sha'])
-                    print("Commit Sha: {}".format(x['sha']))
-                print("\n")
-                currentPage += 1
-        self.totalNumberOfCommits = len(self.commitList)  # Total number of commits in repo
-        self.listOfCCs = []
-        print("Number of commits: {}".format(self.totalNumberOfCommits))
+                for d in data:
+                    self.commitList.append(d['sha'])
+                    print('Commit Sha:', str(d['sha']))
+                currentpage += 1
+
+        self.commitNum = len(self.commitList)
+        self.complexityList = []
+        print('Total number of commits is: ', str(self.commitNum))
 
 
 if __name__ == "__main__":
-    manager = managerServer()  # ini an instance of managerServer()
-    app.run(port=5000)  # int(sys.argv[1])  , debug=True
+    manager = managerServer()
+    app.run(port=5000) 
--- todo-api/flask/worker.py
+++ todo-api/flask/worker.py
@@ -2,45 +2,62 @@ import json, requests, subprocess
 
 def run():
 
-    managerIP = input("Enter the IP of the manager: ")
-    managerPort = input("Enter the port of the manager: ")
     numCommitsDone = 0
     
-    r = requests.get("http://{}:5000/repo".format(managerIP,managerPort), json={'pullStatus': False}) 
-    json_data = json.loads(r.text)
-    repoUrl = json_data['repo']
-    subprocess.call(["bash", "workerInitial.sh", repoUrl])
+    masterURL = 'http://127.0.0.1:5000/repo' 
+    r = requests.get(masterURL, json={'pullStatus': False})
+    data = json.loads(response.text)
+    repoURL = data['repo']
+    bashCommand = "cd pulledRepo &" \
+                  "rm -rf .git/ &" \
+                  "git init &" \
+                  "git remote add origin {} &" \
+                  "git branch --set-upstream-to=origin/<branch> master & " \
+                  "git pull".format(repoUrl)
+    subprocess.Popen(bashCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    r = requests.get("http://{}:5000/repo".format(managerIP,managerPort), json={'pullStatus': True})
+    r = requests.get(masterURL, json={'pullStatus': True})
+
     
-    stillHaveCommits = True
-    while stillHaveCommits:
-        r = requests.get("http://{}:5000/cyclomatic".format(managerIP,managerPort)) # hardcode for now
+    mCommits = True
+    while mCommits:
+        r = requests.get("http://127.0.0.1:5000/cyclomatic")
         json_data = json.loads(r.text)
         print(json_data)
-        print("Received: {}".format(json_data['sha']))
-        if json_data['sha'] == -2:  # Polling for manager to start giving commits
-           print("Polling")
+        hsha = json_data['sha']
+        print("Received: ", str(hsha))
+        if hsha == -2:  # Polling for manager to start giving commits
+           print("Waiting")
         else:
-            if json_data['sha'] == -1:
-                print("No items left")
+            if hsha == -1:
+                print("No more items")
                 break
-            subprocess.call(["bash", "GetCommit.sh", json_data['sha']])
-            binRadonCCOutput = subprocess.check_output(["radon", "cc", "-s", "-a" , "workerData"])
-            radonCCOutput = binRadonCCOutput.decode("utf-8")  # Convert from binary to tring
+            bashCommand = "cd pulledRepo &" \
+                          "git reset --hard {}".format(hsha)
+            subprocess.Popen(bashCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            cmd = "radon cc -a -s workerData"
+            output = subprocess.check_output(cmd).decode()
+            print(output)
 
-            print(radonCCOutput)
-            avgCCstartPos = radonCCOutput.rfind("(")  # Find last open bracket in radon output
-            if radonCCOutput[avgCCstartPos+1:-2] == "":  # There are no files which radon can calculate C.C. for
+            numCommitsDone = 0
+            aveCC = result.rfind('(')
+            if output.find("ERROR") != -1: 
                 print("NO RELEVENT FILES")
-                r = requests.post("http://{}:5000/cyclomatic".format(managerIP,managerPort),
-                                  json={'commitSha': json_data['sha'], 'complexity': -1})
+                r = requests.post("http://127.0.0.1:5000/cyclomatic",
+                                  json={'commit': hsha, 'complexity': 0})
+            elif output[aveCC + 1:-2] == "":
+                print("NO RELEVENT FILES")
+                r = requests.post("http://127.0.0.1:5000/complexity",
+                                  json={'commit': hsha, 'complexity': -1})
             else:
-                averageCC = float(radonCCOutput[avgCCstartPos+1:-2])  # Get the average cyclomatic complexity from the output
-                r = requests.post("http://{}:5000/cyclomatic".format(managerIP,managerPort),
-                                  json={'commitSha': json_data['sha'], 'complexity': averageCC})
+                u = output[result.rfind('(') + 1:-2].replace(')', '').replace('\r', '').replace('\n', '').replace('"', '')
+                if u is None:
+                    r = requests.post('http://127.0.0.1:5000/cyclomatic',
+                                         json={'commit': hsha, 'complexity': 0})
+                else: 
+                    aveCC = float(u)
+                    r = requests.post('http://127.0.0.1:5000/cyclomatic',
+                                         json={'commit': hsha, 'complexity': aveCC})
+
             numCommitsDone += 1  # Increment the number of commits this node has completed
     print("Completed having computed {} commits (including non-computable commits)".format(numCommitsDone))
-
-if __name__ == "__main__":
-    run()
